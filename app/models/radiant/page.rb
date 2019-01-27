45f65d3f0a26ab6d00ed8e8c32976fff4acfd9d7
Benny Degezelle <hi@monkeypatch.be>
2019-01-27 23:11:00 +0100

    Add validations on page title and slug
    (... making 'all' tests green again \o/!)

--- app/models/radiant/page.rb
+++ app/models/radiant/page.rb
@@ -1,5 +1,7 @@
 module Radiant
   class Page < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 255 }
+    validates :slug, presence: true, length: { maximum: 100 }, format: %r{\A([-_.A-Za-z0-9]*|/)\z}, uniqueness: { scope: :parent_id }
     validates :breadcrumb, presence: true, length: { maximum: 160 }
   end
 end
