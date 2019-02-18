c8194ee46e4edb6d5e23f0a5bdeb661971ee4158
Benny Degezelle <hi@monkeypatch.be>
2019-02-18 17:40:43 +1300

    Add class_name validation to pages

--- app/models/radiant/page.rb
+++ app/models/radiant/page.rb
@@ -3,13 +3,29 @@ module Radiant
     validates :title, presence: true, length: { maximum: 255 }
     validates :slug, presence: true, length: { maximum: 100 }, format: %r{\A([-_.A-Za-z0-9]*|/)\z}, uniqueness: { scope: :parent_id }
     validates :breadcrumb, presence: true, length: { maximum: 160 }
+    validates :status_id, presence: true
+
+    validate :valid_class_name
     
+    def self.is_descendant_class_name?(class_name)
+      (Radiant::Page.descendants.map(&:to_s) + [nil, "", "Page"]).include?(class_name)
+    end
+
     def status
-     Radiant::Status.find(self.status_id)
+      Radiant::Status.find(self.status_id)
     end
 
     def status=(value)
       Radiant::self.status_id = value.id
     end
+
+    private
+
+    def valid_class_name
+      unless Radiant::Page.is_descendant_class_name?(class_name)
+        errors.add :class_name, "must be set to a valid descendant of Page"
+      end
+    end
+
   end
 end
--- spec/models/radiant/page_spec.rb
+++ spec/models/radiant/page_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 module Radiant
+  class PageSpecTestPage < Radiant::Page
+  end
+  
   RSpec.describe Page, type: :model do
     let(:page){ build :page }
     
@@ -57,6 +60,56 @@ module Radiant
         other = Page.new(attributes_for(:page).merge(parent_id: 1))
         expect{other.save!}.to raise_error(ActiveRecord::RecordInvalid)
         expect(other.errors[:slug]).to include('has already been taken')
+      end
+    end
+    
+    describe 'title' do
+      it 'is invalid when longer than 255 characters' do
+        page.title = 'x' * 256
+        expect(page).to_not be_valid
+        expect(page.errors[:title]).to include('is too long (maximum is 255 characters)')
+      end
+
+      it 'is invalid when blank' do
+        page.title = ''
+        expect(page).to_not be_valid
+        expect(page.errors[:title]).to include("can't be blank")
+      end
+
+      it 'is valid when 255 characters or shorter' do
+        page.title = 'x' * 255
+        expect(page).to be_valid
+        expect(page.errors[:title]).to be_blank
+      end
+    end
+    
+    describe 'class_name' do
+      it 'should allow mass assignment for class name' do
+        page.attributes = { class_name: 'Radiant::PageSpecTestPage' }
+        expect(page).to be_valid
+        expect(page.errors[:class_name]).to be_blank
+        expect(page.class_name).to be_eql('Radiant::PageSpecTestPage')
+      end
+
+      it 'should not be valid when class name is not a descendant of page' do
+        page.class_name = 'Object'
+        expect(page).to_not be_valid
+        expect(page.errors[:class_name]).to include('must be set to a valid descendant of Page')
+      end
+
+      it 'should not be valid when class name is not a descendant of page and it is set through mass assignment' do
+        page.attributes = { class_name: 'Object' }
+        expect(page).to_not be_valid
+        expect(page.errors[:class_name]).to include('must be set to a valid descendant of Page')
+      end
+
+      it 'should be valid when class name is page or empty or nil' do
+        [nil, '', 'Page'].each do |value|
+          page.class_name = value
+          expect(page).to be_valid
+          expect(page.errors[:class_name]).to be_blank
+          expect(page.class_name).to be_eql(value)
+        end
       end
     end
   end
